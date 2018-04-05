3f6200eac34114c6d8c7708f0a995beeb344c294
Greg Emerick <gemerick@keyholesoftware.com>
2018-04-04 20:57:15 -0500

    Run spring boot as lambda

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
-# Spring boot
+# Spring boot with AWS Lambda
 
-Sample Spring Boot application with a single REST API.
+Sample application to demonstrate running a Spring Boot application as an AWS Lambda built with Cloud formation.
+
+# AWS Lambda
+
+Build the code and deploy with AWS SAM.
+
+mvn clean package
+
+aws cloudformation package --template-file sam.yaml --output-template-file target/output-sam.yaml --s3-bucket lambda-cfn
+ 
+aws cloudformation deploy --template-file target/output-sam.yaml --stack-name spring-boot-lambda --capabilities CAPABILITY_IAM
+ 
+aws cloudformation describe-stacks --stack-name spring-boot-lambda
+
 
 # Run
 
@@ -10,14 +23,14 @@ To build and run from a packaged jar locally:
 
 or 
 
-    mvn clean package
+    mvn clean package -Dboot
     java -jar target/spring-boot-lambda-1.0.0-SNAPSHOT.jar
 
 # Docker
 
 To build the image. First build the application, then build the docker image
 
-    mvn package
+    mvn package -Dboot
     docker build -t spring-boot-lambda .
     
 ## Run
---
+++ src/main/java/com/keyholesoftware/lambda/LambdaHandler.java
@@ -0,0 +1,36 @@
+package com.keyholesoftware.lambda;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+
+public class LambdaHandler implements RequestStreamHandler {
+
+    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    static {
+        try {
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Application.class);
+            handler.activateSpringProfiles("lambda");
+        } catch (ContainerInitializationException e) {
+            // Re-throw the exception to force another cold start
+            e.printStackTrace();
+            throw new RuntimeException("Could not initialize Spring Boot application", e);
+        }
+    }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        handler.proxyStream(inputStream, outputStream, context);
+
+        // just in case it wasn't closed by the mapper
+        outputStream.close();
+    }
+}
