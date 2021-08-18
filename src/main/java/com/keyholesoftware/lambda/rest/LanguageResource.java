9efbf4eb04a2a4c5fc4e26b03845b4a932b72639
rtsui <honfai@trustmechain.com>
2021-08-18 12:41:10 +0800

    update pom, sam

--- src/main/java/com/keyholesoftware/lambda/rest/LanguageResource.java
+++ src/main/java/com/keyholesoftware/lambda/rest/LanguageResource.java
@@ -3,8 +3,7 @@ package com.keyholesoftware.lambda.rest;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.keyholesoftware.lambda.model.Language;
@@ -12,7 +11,7 @@ import com.keyholesoftware.lambda.model.Language;
 @RestController
 public class LanguageResource {
 
-    @RequestMapping(path = "/languages", method = RequestMethod.GET)
+    @GetMapping(path = "/languages")
     public List<Language> listLambdaLanguages() {
         return Arrays.asList(new Language("node"), new Language("java"), new Language("python"));
     }
